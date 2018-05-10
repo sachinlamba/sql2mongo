4e1b471596a4e9f7698546b704bc6274d5e049da
Sachin Lamba <lamba92sachin@gmail.com>
2018-05-10 17:29:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+// mongoose.connect('mongodb://sachin:lamba@ds253587.mlab.com:53587/ppsample');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
 app.use('/', router);
+
 module.exports=app;
+
+//mongo ds253587.mlab.com:53587/ppsample -u sachin -p lamba
