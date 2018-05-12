bb9371e72e556765e50873fa87c0797a2e652322
sachinlamba <lamba92sachin@gmail.com>
2018-05-12 16:02:35 +0530

    mysql msg display update.

--- client/components/App.js
+++ client/components/App.js
@@ -74,7 +74,7 @@ class App extends Component {
       ).then(function(response) {
         console.log("return", response);
         _this.setState({
-          messageFromMySQLServer: response.data.message ? response.data.message : "Error while connection with MySQL.",
+          messageFromMySQLServer: (response.data.message || response.data.code) ? (response.data.message || response.data.code) : "Error while connection with MySQL.",
           connect: response.data.result,
           mysqlConnectTry: false
         });
